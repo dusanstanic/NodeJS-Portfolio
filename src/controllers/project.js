770582d2af91d4ba54b79284c99f0dcabd5fb8bb
Dusan Stanic <dusan.stanic97@hotmail.com>
2021-04-20 22:13:48 +0200

    Add project json

--- src/controllers/project.js
+++ src/controllers/project.js
@@ -1,7 +1,9 @@
 const projectService = require("../services/projects");
 
+const projectss = require("../project.json");
+
 exports.fetchAll = async (req, res, next) => {
   const projects = await projectService.fetchAll();
-  res.json(projects);
+  res.json(projectss);
 };
-    // "start": "nodemon src/app.js",
+// "start": "nodemon src/app.js",
